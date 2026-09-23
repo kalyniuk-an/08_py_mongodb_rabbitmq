bb8d8a826977a24167f9cfecdeeecc11ae04d669
Anastasiia Kalyniuk <Kalyniuk.an@gmail.com>
2026-09-23 08:56:23 -0500

    connect

---
+++ connect.py
@@ -0,0 +1,13 @@
+import os
+
+from dotenv import load_dotenv
+from mongoengine import connect
+
+
+load_dotenv()
+
+MONGO_URI = os.getenv("MONGO_URI")
+
+connect(host=MONGO_URI)
+
+print("MongoDB connected!")
